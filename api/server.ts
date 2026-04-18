7ddc7927c881b27078e934acd44e5c2402453c4e
Arijit <arijitpal2350@gmail.com>
2026-04-18 11:23:35 +0530

    fix: include required .js extension for ESM imports in api/

--- api/server.ts
+++ api/server.ts
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import pool from "./db";
+import pool from "./db.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
